3339f50513049f7a79b6eafda3c74f38e9136577
Joseph Pickard <pickardjoe@gmail.com>
2017-09-18 17:31:54 -0400

    checkpoint

--- control/sendcontrol.js
+++ control/sendcontrol.js
@@ -1,12 +1,12 @@
 var TxControl = require('./TxControl');
 
 
-var ctl = new TxControl(process.argv[2], 115200, function(ctl1) {
-    ctl1.debug = ['all'];
-    ctl1.OnTelemetry(function(telemData) {
+var ctl = new TxControl(process.argv[2], 115200, function() {
+    ctl.debug = ['all'];
+    ctl.OnTelemetry(function(telemData) {
         console.log(JSON.stringify(telemData));
     });
-    ctl1.SendLoop(function() {
-        ctl1.Stop();
+    ctl.SendLoop(function() {
+        ctl.Stop();
     });
 });
--- control/sendcontrolx52.js
+++ control/sendcontrolx52.js
@@ -1,13 +1,13 @@
 var TxControl = require('./TxControl');
 
 
-var ctl = new TxControl(process.argv[2], 115200, function(ctl1) {
-    ctl1.debug = ['all'];
-    ctl1.OnTelemetry(function(telemData) {
+var ctl = new TxControl(process.argv[2], 115200, function(ctl) {
+    ctl.debug = ['all'];
+    ctl.OnTelemetry(function(telemData) {
         console.log(JSON.stringify(telemData));
     });
-	ctl1.channelMap = {0 : 2, 1: 3, 2: -1, 5: 4};
-    ctl1.SendLoop(function() {
-        ctl1.Stop();
+	ctl.channelMap = {0 : 2, 1: 3, 2: -1, 5: 4};
+    ctl.SendLoop(function() {
+        ctl.Stop();
     });
 });
